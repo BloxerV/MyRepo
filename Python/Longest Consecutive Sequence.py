0fe686dac7df4d96c897bbb223d252eb7d48cdb8
Bloxer <mpjoker70@gmail.com>
2024-08-20 00:12:38 +0200

    solved O(n)

--- Python/Longest Consecutive Sequence.py
+++ Python/Longest Consecutive Sequence.py
@@ -1,14 +1,11 @@
-from collections import defaultdict
-
 nums=[2,20,4,10,3,4,5]
-sorted_nums = list(sorted(dict.fromkeys(nums)))
-nums_consecutive = [1] * len(sorted_nums)
-nums_consecutive_sets = defaultdict(set)
-key = 0
+nums_consecutive_sets = set(nums)
+longest = 0
 
-for i in range(len(sorted_nums)):
-    nums_consecutive_sets[key].add(sorted_nums[i])
-    if i < len(sorted_nums) - 1 and sorted_nums[i] + 1 != sorted_nums[i + 1]:
-        key +=1
-
-print(max(len(item) for item in nums_consecutive_sets.values()))
+for i in nums_consecutive_sets:
+    if i - 1 not in nums_consecutive_sets:
+        lenght = 1
+        while (i + lenght) in nums_consecutive_sets:
+            lenght += 1
+        longest = max(longest, lenght)
+print(longest)
