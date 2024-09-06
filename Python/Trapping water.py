51d6da2970d0a7dbfaa4807ba4082212aa957ffc
Bloxer <mpjoker70@gmail.com>
2024-09-06 12:25:33 +0200

    Code

--- Python/Trapping water.py
+++ Python/Trapping water.py
@@ -1,13 +1,31 @@
-def trap(self, height: List[int]) -> int:
-    left, right = 0, len(heights) - 1
-    suma = 0
-
+def trap(height: list[int]) -> int:
+    left, right = 0, len(height) - 1
+    res = 0
 
     while left < right:
-        res = (right - left) * min(heights[right], heights[left])
+
+        if  height[left] > height[left + 1]:
+            res += height[left] - height[left + 1]
+            left += 1
+
+            while left < right and height[left] < height[left + 1]:
+                left += 1
+                res += height[left] - height[left + 1]
+
+        else:
+            left += 1
+            
+
+    return res
+
+print(trap([0,2,0,3,1,0,1,3,2,1]))
+        
+'''
+    while left < right:
+        res = (right - left) * min(height[right], height[left])
         suma = max(suma, res)
 
-        if heights[left] == 0:
+        if height[left] == 0:
             left += 1
 
         else:
@@ -16,17 +34,10 @@ def trap(self, height: List[int]) -> int:
     return suma
 Jeśli lewy jest większy od lewgo + 1 to res = left - (left +1): 
 aktualizacj o każdy skok res += left - (left +1) dopóki nowy lewy nie będzie równy bądź większy od lewego od którego to się zaczeło
-while left < right:
-    if  heights[left] > heights[left + 1]:
-        res += heights[left] - height[left + 1]
-        left += 1
-        while heights[left] < heights[left + 1]
-            left += 1   
-        
-
+'''
 
 '''
-You are given an array non-negative integers heights which represent an elevation map. Each value heights[i] represents the height of a bar, which has a width of 1.
+You are given an array non-negative integers height which represent an elevation map. Each value height[i] represents the height of a bar, which has a width of 1.
 
 Return the maximum area of water that can be trapped between the bars.
 
