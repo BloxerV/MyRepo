fe28b3af3b8346d07ab863a3353d4651334e9f73
Bloxer <mpjoker70@gmail.com>
2024-09-08 18:29:27 +0200

    Code

--- Python/Validate Parentheses.py
+++ Python/Validate Parentheses.py
@@ -1,6 +1,15 @@
 def isValid(s: str) -> bool:
+    left, right = 0, len(s) - 1
 
-
+    while left < right:
+        if s[left] == s[right]:
+            left += 1
+            right -= 1
+        else:
+            return 'The brackets are not closed in the correct order.'
+    return True
+st = "([{}])"
+print(isValid(st))
 
 '''
 You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
